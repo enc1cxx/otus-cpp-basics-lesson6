b8176c3374c8d3f96244c6912f436c9861ee58e3
Aleksey Aleksandrov <yohoho4721@gmail.com>
2023-09-21 07:55:20 +0300

    ptr instead Node

--- main.cpp
+++ main.cpp
@@ -1,44 +1,51 @@
-#include "single_linked_list.hpp"
 #include "double_linked_list.hpp"
+#include "single_linked_list.hpp"
 #include "sll.hpp"
 
 int main() {
-    single_linked_list::SingleLinkedList<int> list1;
-    std::cout << " list1.size() = " << list1.size() << std::endl;
-    list1.print();
+  // 1
+  single_linked_list::SingleLinkedList<int> list1;
+  // 2
+  for (int i = 0; i < 10; ++i) {
+    list1.push_back(i);
+  }
+  // 3
+  std::cout << "result print() = ";
+  list1.print();
+  // 4
+  std::cout << " list1.size() = " << list1.size() << std::endl;
+  // 5
+  list1.erase(list1.begin());
+  //   list1.erase(std::next(list1.begin(), 3));
+  //   list1.erase(std::next(list1.begin(), 4));
+  // 6
+  std::cout << "result print() = ";
+  list1.print();
 
-    list1.push_back(1); 
-    std::cout << " list1.size() = " << list1.size() << std::endl;
-    list1.print();
+  single_linked_list::SingleLinkedList<int> list2;
+  list2 = list1;
 
-    list1.push_back(2);
-    std::cout << " list1.size() = " << list1.size() << std::endl;
-    list1.print();
+  std::cout << "result print() = ";
+  list2.print();
 
-    list1.push_back(3);
-    std::cout << " list1.size() = " << list1.size() << std::endl;
-    list1.print();  
-    //============================================
+//   std::cout << "================" << std::endl;
 
-    // single_linked_list::SingleLinkedList<int> list2;
-    // std::cout << " list2.size() = " << list2.size() << std::endl;
-    // list2.print();
+//   // 1
+//   single_linked_list::SingleLinkedList<int> list2 = {0, 1, 2, 3, 4,
+//                                                      5, 6, 7, 8, 9};
 
-    // list2.push_front(3); 
-    // std::cout << " list2.size() = " << list2.size() << std::endl;
-    // list2.print();
+//   // 3
+//   std::cout << "result print() = ";
+//   list2.print();
+//   // 4
+//   std::cout << " list2.size() = " << list2.size() << std::endl;
+//   // 5
+//   list2.erase(list2.before_begin());
+//   //   list1.erase(std::next(list1.begin(), 3));
+//   //   list1.erase(std::next(list1.begin(), 4));
+//   // 6
+//   std::cout << "result print() = ";
+//   list2.print();
 
-    // list2.push_front(2);
-    // std::cout << " list2.size() = " << list2.size() << std::endl;
-    // list2.print();
-
-    // list2.push_front(1);
-    // std::cout << " list2.size() = " << list2.size() << std::endl;
-    // list2.print();
-
-
-    // std::cout << (list1 == list2) << std::endl;
-
-
-    return 0;
+  return 0;
 }
